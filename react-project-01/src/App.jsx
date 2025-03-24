2cd7176dcfdc34291c34da70dc016611398166ee
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-03-24 23:10:52 +0530

    Project 01

---
+++ react-project-01/src/App.jsx
@@ -0,0 +1,12 @@
+import UserCard from "./components/UserCard";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <UserCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-project-01/src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserCard = () => {
+  return (
+    <div className="UserContainer">
+      <h3 id='userName'>Abhijeet Singh</h3>
+      <p id='userAge'>I am 100 years old.</p>
+      <p id='userDescription'>I am learning React JS.</p>
+    </div>
+  );
+};
+
+export default UserCard; // 
