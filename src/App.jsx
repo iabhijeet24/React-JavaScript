180172c3a01cc9f4819f5533f3883c69a0dbfbd3
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-03-25 23:57:59 +0530

    Second Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import UserCard from "./components/UserCard";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, React!</h1>
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './UserCardStyle.css'
+
+const UserCard = () => {
+  return (
+    <div className="UserContainer">
+      <h3 id='userName'>Abhijeet Singh</h3>
+      <p id='userAge'>I am 100 years old.</p>
+      <p id='userDescription'>I am learning React JS.</p>
+    </div>
+  );
+};
+
+export default UserCard; // 
